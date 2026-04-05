ce9ff6296905ecdd5d980e058f232051c83adf92
Garima Kashyap <garimakashyap2600@gmail.com>
2026-04-05 23:44:48 +0530

    fix cors issue final

--- backend/app.js
+++ backend/app.js
@@ -5,32 +5,31 @@ import hospitalRoutes from "./routes/hospitalRoutes.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 import messageRouter from "./router/messageRouter.js";
-
-
 import feedbackRoutes from "./routes/feedbackRoutes.js";
 
 const app = express();
 
-// ✅ CORS FIX
+// ✅ FIXED CORS
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
-// 🔥 VERY IMPORTANT LINE
-app.options("*", cors());
+app.options("*", cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/api/v1/hospitals", hospitalRoutes);
+
 // ROUTES
+app.use("/api/v1/hospitals", hospitalRoutes);
 app.use("/api/v1/feedback", feedbackRoutes);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 app.use("/api/v1/message", messageRouter);
-
-
 
 export default app;
