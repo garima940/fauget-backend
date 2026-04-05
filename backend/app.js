7d5146a63045f10b82513e638ff009fe5a6c49c9
Garima Kashyap <garimakashyap2600@gmail.com>
2026-04-05 16:00:37 +0530

    fixed cors for production

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ✅ CORS FIX
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
